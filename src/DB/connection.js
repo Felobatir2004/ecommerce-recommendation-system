2640be32e28667e8b940de3d0b048ae238bc40b5
Felobatir2004 <felomories99@gmail.com>
2025-06-04 00:44:34 +0200

    g

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     try {
                 console.log("Connecting to DB:", process.env.DB_URI);
-        await mongoose.connect(process.env.DB_URI , {
-            serverSelectionTimeoutMS: 5000,
-        })
+await mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+});
         console.log("connected to DB")
     } catch (error) {
         console.log("Error connecting to DB" , error);
