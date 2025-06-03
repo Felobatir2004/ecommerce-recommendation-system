496db79f491e0c74a613a7612323547a8a365723
Felobatir2004 <felomories99@gmail.com>
2025-06-03 21:29:31 +0200

    dfc

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.DB_URI , {
-            serverSelectionTimeoutMS: 5000,
+              serverSelectionTimeoutMS: 30000, // 30 ثانية
+            socketTimeoutMS: 45000, // 45 ثانية       
         })
         console.log("connected to DB")
     } catch (error) {
