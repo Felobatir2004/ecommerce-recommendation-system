f70aee125287c7f149b07e499d0be284637949ef
Felobatir2004 <felomories99@gmail.com>
2025-06-03 02:18:19 +0200

    m

--- src/Modules/withlist/withlist.controller.js
+++ src/Modules/withlist/withlist.controller.js
@@ -3,6 +3,6 @@ import * as wishlist from './services/withlist.service.js'
 import { authentication } from "../../middlewares/auth.middleware.js";
 const router=Router()
 router.patch('/addtowithlist',authentication(),wishlist.addtowithlist)
-router.delete('/removefromwithlist/:id',wishlist.removefromwithlist)
-router.get('/getallinwithlist',wishlist.getallinwithlist)
+router.delete('/removefromwithlist/:id',authentication(),wishlist.removefromwithlist)
+router.get('/getallinwithlist',authentication(),wishlist.getallinwithlist)
 export default router
