344cb937afbe1dc99fb6d0a086f6f819d1645b5a
Felobatir2004 <felomories99@gmail.com>
2025-06-12 16:56:19 +0200

    dcd

---
+++ src/DB/Models/contact.model.js
@@ -0,0 +1,31 @@
+
+import mongoose, { Schema, model } from "mongoose";
+
+const contactSchema = new Schema(
+{
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ContactModel = mongoose.models.Contact || model("Contact", contactSchema);
