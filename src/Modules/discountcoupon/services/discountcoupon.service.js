13bfee5f5dcc6f9e1f611c913a8eed0a5c9c19aa
AbdelrahmanEssam17 <eabdo6990@gmail.com>
2025-06-14 17:21:14 +0300

    ;

--- src/Modules/discountcoupon/services/discountcoupon.service.js
+++ src/Modules/discountcoupon/services/discountcoupon.service.js
@@ -42,6 +42,7 @@ export const deletediscountcoupon=async(req,res,next)=>{
 import axios from 'axios';
 
 export const getCollaborativeRecommendations = async (req, res, next) => {
+    k
   const { user_id } = req.query;
   if (!user_id) {
     return res.status(400).json({ message: "user_id is required" });
