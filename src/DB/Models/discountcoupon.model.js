affa5bac0955670f7528973b3a672a611c0aa94d
AbdelrahmanEssam17 <eabdo6990@gmail.com>
2025-06-17 00:31:38 +0300

    ads

--- src/DB/Models/discountcoupon.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose,{model, Schema} from "mongoose";
-const coupondiscountschema=new Schema({
-    code:String,
-    endDate:Date,
-discountpercentage:Number,
-},
-    {timestamps:true})
-    export const coupon=mongoose.models.coupon||model("coupon",coupondiscountschema)
